dd7d0ef2fd5402dc2be903b4e616877f2a92337b
Caleb Taber <calebtaber@gmail.com>
2023-08-01 20:22:29 -0400

    Fixed bug where calendar would close when selected_date was set to the default (01/01/1970)

--- DatePicker.py
+++ DatePicker.py
@@ -4,10 +4,11 @@ import datetime
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
+DATE_DEFAULT = datetime.date(year=1970, month=1, day=1)
 
 class DatePicker(Gtk.Box):
     calendar = Gtk.Calendar()
-    selected_date = datetime.date(year=1970, month=1, day=1)
+    selected_date = DATE_DEFAULT
     select_button = Gtk.Button(visible=False)
 
     def __init__(self):
@@ -16,14 +17,10 @@ class DatePicker(Gtk.Box):
 
         self.calendar.set_visible(True)
         self.calendar.connect("day-selected", self.on_date_chosen)
-        self.calendar.connect("month-changed", self.clear_selection)
         self.add(self.calendar)
 
         self.select_button.connect("clicked", self.on_choose_date)
         self.add(self.select_button)
-
-    def clear_selection(self, widget):
-        self.calendar.clear_marks()
 
     def on_choose_date(self, widget):
         self.select_button.set_visible(False)
@@ -34,7 +31,8 @@ class DatePicker(Gtk.Box):
         selection = datetime.date(year=date_tuple[0], month=date_tuple[1] + 1, day=date_tuple[2])
 
         # If the user is not just changing the month in the calendar, set the current selection as the date
-        if not ((selection.month == self.selected_date.month - 1 or
+        if not (self.selected_date == DATE_DEFAULT or
+                (selection.month == self.selected_date.month - 1 or
                  selection.year == self.selected_date.year - 1) or
                 (selection.month == self.selected_date.month + 1 or
                  selection.year == self.selected_date.year + 1)):
